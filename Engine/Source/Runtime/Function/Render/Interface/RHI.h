2b59a27df6219d4827319f936a3c05a2e16622e4
HUAWEI\Administrator <945173861@qq.com>
2025-07-21 20:47:11 +0800

    feat: RHI的接口补充

--- Engine/Source/Runtime/Function/Render/Interface/RHI.h
+++ Engine/Source/Runtime/Function/Render/Interface/RHI.h
@@ -2,9 +2,17 @@
 #include <vector>
 #include <Common.h>
 #include <vk_mem_alloc.h>
+#include <functional>
 #include "RHIStruct.h"
 
 NAMESPACE_XYH_BEGIN
+
+class WindowSystem;
+
+struct RHIInitInfo
+{
+	std::shared_ptr<WindowSystem> m_pWindowSystem;
+};
 
 class RHI
 {
@@ -16,6 +24,7 @@ public:
 
 	virtual bool IsPointLightShadowEnabled() = 0;	// 是否启用点光源阴影
 
+	// 分配和创建
 	virtual bool AllocateCommandBuffer() = 0;	// 配置命令缓冲区
 	virtual bool AllocateDescriptorSet() = 0;	// 配置描述符集
 	virtual void CreateSwapChain() = 0;	// 创建交换链
@@ -27,23 +36,191 @@ public:
 	virtual RHIShader* CreateShaderModule(const std::vector<unsigned char>& shaderCode) = 0;	// 创建着色器模块
 	virtual void CreateBuffer(RHIDeviceSize size, RHIBufferUsageFlags usage, RHIMemoryPropertyFlags properties, RHIBuffer*& buffer, RHIDeviceMemory*& bufferMemory) = 0;	// 创建缓冲区
 	virtual void CreateBufferAndInitialize(RHIBufferUsageFlags usage, RHIMemoryPropertyFlags properties, RHIBuffer*& buffer, RHIDeviceMemory*& bufferMemory, RHIDeviceSize size, void* data = nullptr, int datasize = 0) = 0;	// 创建缓冲区并初始化
-	virtual bool createBufferVMA(VmaAllocator allocator,
+	
+	virtual bool CreateBufferVMA(VmaAllocator allocator,
 		const RHIBufferCreateInfo* pBufferCreateInfo,
 		const VmaAllocationCreateInfo* pAllocationCreateInfo,
 		RHIBuffer*& pBuffer,
 		VmaAllocation* pAllocation,
 		VmaAllocationInfo* pAllocationInfo) = 0;	// 使用VMA创建缓冲区
 
-private:
+	virtual bool CreateBufferWithAlignmentVMA(
+		VmaAllocator allocator,
+		const RHIBufferCreateInfo* pBufferCreateInfo,
+		const VmaAllocationCreateInfo* pAllocationCreateInfo,
+		RHIDeviceSize minAlignment,
+		RHIBuffer*& pBuffer,
+		VmaAllocation* pAllocation,
+		VmaAllocationInfo* pAllocationInfo) = 0;	// 使用VMA创建缓冲区并对齐
+
+	virtual void CopyBuffer(RHIBuffer* srcBuffer, RHIBuffer* dstBuffer, RHIDeviceSize srcOffset, RHIDeviceSize dstOffset, RHIDeviceSize size) = 0;	// 复制缓冲区
+
+	virtual void CreateImage(uint32_t image_width, uint32_t image_height, ERHIFormat format, ERHIImageTiling image_tiling, RHIImageUsageFlags image_usage_flags, RHIMemoryPropertyFlags memory_property_flags,
+		RHIImage*& image, RHIDeviceMemory*& memory, RHIImageCreateFlags image_create_flags, uint32_t array_layers, uint32_t miplevels) = 0;	// 创建图像
+
+	virtual void CreateImageView(RHIImage* image, ERHIFormat format, RHIImageAspectFlags image_aspect_flags, ERHIImageViewType view_type, uint32_t layout_count, uint32_t miplevels,
+		RHIImageView*& image_view) = 0;	// 创建图像视图
+
+	virtual void CreateGlobalImage(RHIImage*& image, RHIImageView*& image_view, VmaAllocation& image_allocation, uint32_t texture_image_width, uint32_t texture_image_height, void* texture_image_pixels, 
+		ERHIFormat texture_image_format, uint32_t miplevels = 0) = 0;	// 创建全局图像
+
+	virtual void CreateCubeMap(RHIImage*& image, RHIImageView*& image_view, VmaAllocation& image_allocation, uint32_t texture_image_width, uint32_t texture_image_height, std::array<void*, 6> texture_image_pixels, 
+		ERHIFormat texture_image_format, uint32_t miplevels) = 0;	// 创建立方体贴图
+
+	virtual void CreateCommandPool() = 0;	// 创建命令池
+
+	virtual bool CreateCommandPool(const RHICommandPoolCreateInfo* pCreateInfo, RHICommandPool*& pCommandPool) = 0;	// 创建命令池
+
+	virtual bool CreateDescriptorPool(const RHIDescriptorPoolCreateInfo* pCreateInfo, RHIDescriptorPool*& pDescriptorPool) = 0;	// 创建描述符池
+
+	virtual bool CreateDescriptorSetLayout(const RHIDescriptorSetLayoutCreateInfo* pCreateInfo, RHIDescriptorSetLayout*& pSetLayout) = 0;	// 创建描述符集布局
+
+	virtual bool CreateFence(const RHIFenceCreateInfo* pCreateInfo, RHIFence*& pFence) = 0;
+
+	virtual bool CreateFramebuffer(const RHIFramebufferCreateInfo* pCreateInfo, RHIFramebuffer*& pFramebuffer) = 0;
+
+	virtual bool CreateGraphicsPipelines(RHIPipelineCache* pipelineCache, uint32_t createInfoCount, const RHIGraphicsPipelineCreateInfo* pCreateInfos, RHIPipeline*& pPipelines) = 0;
+
+	virtual bool CreateComputePipelines(RHIPipelineCache* pipelineCache, uint32_t createInfoCount, const RHIComputePipelineCreateInfo* pCreateInfos, RHIPipeline*& pPipelines) = 0;
+
+	virtual bool CreatePipelineLayout(const RHIPipelineLayoutCreateInfo* pCreateInfo, RHIPipelineLayout*& pPipelineLayout) = 0;
+
+	virtual bool CreateRenderPass(const RHIRenderPassCreateInfo* pCreateInfo, RHIRenderPass*& pRenderPass) = 0;
+
+	virtual bool CreateSampler(const RHISamplerCreateInfo* pCreateInfo, RHISampler*& pSampler) = 0;
+
+	virtual bool CreateSemaphore(const RHISemaphoreCreateInfo* pCreateInfo, RHISemaphore*& pSemaphore) = 0;
+
+
+	// command and command write
+	virtual bool WaitForFencesPFN(uint32_t fenceCount, RHIFence* const* pFence, RHIBool32 waitAll, uint64_t timeout) = 0;	// 等待栅栏
+
+	virtual bool ResetFencesPFN(uint32_t fenceCount, RHIFence* const* pFences) = 0;	// 重置栅栏
+
+	virtual bool ResetCommandPoolPFN(RHICommandPool* commandPool, RHICommandPoolResetFlags flags) = 0;	// 
+
+	virtual bool BeginCommandBufferPFN(RHICommandBuffer* commandBuffer, const RHICommandBufferBeginInfo* pBeginInfo) = 0;
+
+	virtual bool EndCommandBufferPFN(RHICommandBuffer* commandBuffer) = 0;
+
+	virtual void CmdBeginRenderPassPFN(RHICommandBuffer* commandBuffer, const RHIRenderPassBeginInfo* pRenderPassBegin, ERHISubpassContents contents) = 0;
+
+	virtual void CmdNextSubpassPFN(RHICommandBuffer* commandBuffer, ERHISubpassContents contents) = 0;
+
+	virtual void CmdEndRenderPassPFN(RHICommandBuffer* commandBuffer) = 0;
+
+	virtual void CmdBindPipelinePFN(RHICommandBuffer* commandBuffer, ERHIPipelineBindPoint pipelineBindPoint, RHIPipeline* pipeline) = 0;
+
+	virtual void CmdSetViewportPFN(RHICommandBuffer* commandBuffer, uint32_t firstViewport, uint32_t viewportCount, const ST_RHIViewport* pViewports) = 0;
+
+	virtual void CmdSetScissorPFN(RHICommandBuffer* commandBuffer, uint32_t firstScissor, uint32_t scissorCount, const ST_RHIRect2D* pScissors) = 0;
+
+	virtual void CmdBindVertexBuffersPFN(
+		RHICommandBuffer* commandBuffer,
+		uint32_t firstBinding,
+		uint32_t bindingCount,
+		RHIBuffer* const* pBuffers,
+		const RHIDeviceSize* pOffsets) = 0;
+
+	virtual void CmdBindIndexBufferPFN(RHICommandBuffer* commandBuffer, RHIBuffer* buffer, RHIDeviceSize offset, ERHIIndexType indexType) = 0;
+
+	virtual void CmdBindDescriptorSetsPFN(
+		RHICommandBuffer* commandBuffer,
+		ERHIPipelineBindPoint pipelineBindPoint,
+		RHIPipelineLayout* layout,
+		uint32_t firstSet,
+		uint32_t descriptorSetCount,
+		const RHIDescriptorSet* const* pDescriptorSets,
+		uint32_t dynamicOffsetCount,
+		const uint32_t* pDynamicOffsets) = 0;
+
+	virtual void CmdDrawIndexedPFN(RHICommandBuffer* commandBuffer, uint32_t indexCount, uint32_t instanceCount, uint32_t firstIndex, int32_t vertexOffset, uint32_t firstInstance) = 0;
+
+	virtual void CmdClearAttachmentsPFN(RHICommandBuffer* commandBuffer, uint32_t attachmentCount, const ST_RHIClearAttachment* pAttachments, uint32_t rectCount, const ST_RHIClearRect* pRects) = 0;
+
+	virtual bool BeginCommandBuffer(RHICommandBuffer* commandBuffer, const RHICommandBufferBeginInfo* pBeginInfo) = 0;
+	virtual void CmdCopyImageToBuffer(RHICommandBuffer* commandBuffer, RHIImage* srcImage, ERHIImageLayout srcImageLayout, RHIBuffer* dstBuffer, uint32_t regionCount, const RHIBufferImageCopy* pRegions) = 0;
+
+	virtual void CmdCopyImageToImage(RHICommandBuffer* commandBuffer, RHIImage* srcImage, ERHIImageAspectFlagBits srcFlag, RHIImage* dstImage, ERHIImageAspectFlagBits dstFlag, uint32_t width, uint32_t height) = 0;
+
+	virtual void CmdCopyBuffer(RHICommandBuffer* commandBuffer, RHIBuffer* srcBuffer, RHIBuffer* dstBuffer, uint32_t regionCount, RHIBufferCopy* pRegions) = 0;
+
+	virtual void CmdDraw(RHICommandBuffer* commandBuffer, uint32_t vertexCount, uint32_t instanceCount, uint32_t firstVertex, uint32_t firstInstance) = 0;
+
+	virtual void CmdDispatch(RHICommandBuffer* commandBuffer, uint32_t groupCountX, uint32_t groupCountY, uint32_t groupCountZ) = 0;
+
+	virtual void CmdDispatchIndirect(RHICommandBuffer* commandBuffer, RHIBuffer* buffer, RHIDeviceSize offset) = 0;
+
+	virtual void CmdPipelineBarrier(RHICommandBuffer* commandBuffer, RHIPipelineStageFlags srcStageMask, RHIPipelineStageFlags dstStageMask, RHIDependencyFlags dependencyFlags, uint32_t memoryBarrierCount, 
+		const ST_RHIMemoryBarrier* pMemoryBarriers, uint32_t bufferMemoryBarrierCount, const RHIBufferMemoryBarrier* pBufferMemoryBarriers, uint32_t imageMemoryBarrierCount, 
+		const RHIImageMemoryBarrier* pImageMemoryBarriers) = 0;
+
+	virtual bool EndCommandBuffer(RHICommandBuffer* commandBuffer) = 0;
+
+	virtual void UpdateDescriptorSets(uint32_t descriptorWriteCount, const RHIWriteDescriptorSet* pDescriptorWrites, uint32_t descriptorCopyCount, const RHICopyDescriptorSet* pDescriptorCopies) = 0;
+
+	virtual bool QueueSubmit(RHIQueue* queue, uint32_t submitCount, const RHISubmitInfo* pSubmits, RHIFence* fence) = 0;
+
+	virtual bool QueueWaitIdle(RHIQueue* queue) = 0;
+
+	virtual void ResetCommandPool() = 0;
+
+	virtual void WaitForFences() = 0;
+
+	// query
+	virtual void GetPhysicalDeviceProperties(RHIPhysicalDeviceProperties* pProperties) = 0;
+	virtual RHICommandBuffer* GetCurrentCommandBuffer() const = 0;
+	virtual RHICommandBuffer* const* GetCommandBufferList() const = 0;
+	virtual RHICommandPool* GetCommandPoor() const = 0;
+	virtual RHIDescriptorPool* GetDescriptorPoor() const = 0;
+	virtual RHIFence* const* GetFenceList() const = 0;
+	virtual ST_QueueFamilyIndices GetQueueFamilyIndices() const = 0;
+	virtual RHIQueue* GetGraphicsQueue() const = 0;
+	virtual RHIQueue* GetComputeQueue() const = 0;
+	virtual ST_RHISwapChainDesc GetSwapchainInfo() = 0;
+	virtual ST_RHIDepthImageDesc GetDepthImageInfo() const = 0;
+	virtual uint8_t GetMaxFramesInFlight() const = 0;
+	virtual uint8_t GetCurrentFrameIndex() const = 0;
+	virtual void SetCurrentFrameIndex(uint8_t index) = 0;
+
+	// command write
+	virtual RHICommandBuffer* BeginSingleTimeCommands() = 0;	// 
+	virtual void EndSingleTimeCommands(RHICommandBuffer* command_buffer) = 0;
+	virtual bool PrepareBeforePass(std::function<void()> passUpdateAfterRecreateSwapchain) = 0;
+	virtual void SubmitRendering(std::function<void()> passUpdateAfterRecreateSwapchain) = 0;
+	virtual void PushEvent(RHICommandBuffer* commond_buffer, const char* name, const float* color) = 0;
+	virtual void PopEvent(RHICommandBuffer* commond_buffer) = 0;
+
+	// destory
+	virtual void Clear() = 0;	// 
+	virtual void ClearSwapchain() = 0;	// 
+	virtual void DestroyDefaultSampler(ERHIDefaultSamplerType type) = 0;
+	virtual void DestroyMipmappedSampler() = 0;
+	virtual void DestroyShaderModule(RHIShader* shader) = 0;
+	virtual void DestroySemaphore(RHISemaphore* semaphore) = 0;
+	virtual void DestroySampler(RHISampler* sampler) = 0;
+	virtual void DestroyInstance(RHIInstance* instance) = 0;
+	virtual void DestroyImageView(RHIImageView* imageView) = 0;
+	virtual void DestroyImage(RHIImage* image) = 0;
+	virtual void DestroyFramebuffer(RHIFramebuffer* framebuffer) = 0;
+	virtual void DestroyFence(RHIFence* fence) = 0;
+	virtual void DestroyDevice() = 0;
+	virtual void DestroyCommandPool(RHICommandPool* commandPool) = 0;
+	virtual void DestroyBuffer(RHIBuffer*& buffer) = 0;
+	virtual void FreeCommandBuffers(RHICommandPool* commandPool, uint32_t commandBufferCount, RHICommandBuffer* pCommandBuffers) = 0;
+
+	// memory
+	virtual void FreeMemory(RHIDeviceMemory*& memory) = 0;
+	virtual bool MapMemory(RHIDeviceMemory* memory, RHIDeviceSize offset, RHIDeviceSize size, RHIMemoryMapFlags flags, void** ppData) = 0;
+	virtual void UnmapMemory(RHIDeviceMemory* memory) = 0;
+	virtual void InvalidateMappedMemoryRanges(void* pNext, RHIDeviceMemory* memory, RHIDeviceSize offset, RHIDeviceSize size) = 0;
+	virtual void FlushMappedMemoryRanges(void* pNext, RHIDeviceMemory* memory, RHIDeviceSize offset, RHIDeviceSize size) = 0;
+
+	//semaphores
+	virtual RHISemaphore*& GetTextureCopySemaphore(uint32_t index) = 0;
 
 };
 
-RHI::RHI()
-{
-}
-
-RHI::~RHI()
-{
-}
+inline RHI::~RHI() = default;
 
 NAMESPACE_XYH_END
--- Engine/Source/Runtime/Function/Render/Interface/RHIStruct.h
+++ Engine/Source/Runtime/Function/Render/Interface/RHIStruct.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Common.h>
+#include <optional>
 #include "../RenderType.h"
 
 NAMESPACE_XYH_BEGIN
@@ -34,16 +35,16 @@ class RHIShader {};	// 着色器
 
 
 ////////////////////结构体//////////////////////////
-struct RHIMemoryBarrier;	// 内存屏障
+struct ST_RHIMemoryBarrier;	// 内存屏障
 struct RHICopyDescriptorSet;
 struct RHIDescriptorImageInfo;
 struct RHIDescriptorBufferInfo;
-struct RHIOffset2D;
+struct ST_RHIOffset2D;
 struct RHISpecializationMapEntry;
 struct RHIBufferMemoryBarrier;
 struct RHIImageSubresourceRange;
 struct RHIImageMemoryBarrier;
-struct RHIExtent2D;
+struct ST_RHIExtent2D;
 struct RHIExtent3D;
 struct RHIApplicationInfo;
 struct RHIAttachmentDescription;
@@ -108,19 +109,19 @@ struct RHIAttachmentReference;
 struct RHIComponentMapping;
 struct RHIImageSubresourceLayers;
 struct RHIPipelineColorBlendAttachmentState;
-struct RHIRect2D;
+struct ST_RHIRect2D;
 struct RHISpecializationInfo;
 struct RHIStencilOpState;
 struct RHIVertexInputAttributeDescription;
 struct RHIVertexInputBindingDescription;
-struct RHIViewport;
+struct ST_RHIViewport;
 struct RHIRenderPassBeginInfo;
-union RHIClearValue;
-union RHIClearColorValue;
-struct RHIClearDepthStencilValue;
+union UN_RHIClearValue;
+union UN_RHIClearColorValue;
+struct ST_RHIClearDepthStencilValue;
 
 ////////////////////结构体定义////////////////////////
-struct RHIMemoryBarrier 
+struct ST_RHIMemoryBarrier
 {
     ERHIStructureType m_type;
     const void* m_pNext;
@@ -128,5 +129,90 @@ struct RHIMemoryBarrier
     RHIAccessFlags m_dstAccessMask;
 };
 
+struct ST_RHIClearAttachment
+{
+    RHIImageAspectFlags aspectMask;
+    uint32_t colorAttachment;
+    UN_RHIClearValue clearValue;
+};
+
+union UN_RHIClearValue
+{
+    UN_RHIClearColorValue color;
+    ST_RHIClearDepthStencilValue depthStencil;
+};
+
+union UN_RHIClearColorValue 
+{
+    float float32[4];
+    int32_t int32[4];
+    uint32_t uint32[4];
+};
+
+struct ST_RHIClearDepthStencilValue 
+{
+    float depth;
+    uint32_t stencil;
+};
+
+struct ST_RHIClearRect 
+{
+    ST_RHIRect2D rect;
+    uint32_t baseArrayLayer;
+    uint32_t layerCount;
+};
+
+struct ST_RHIRect2D
+{
+    ST_RHIOffset2D offset;
+    ST_RHIExtent2D extent;
+};
+
+struct ST_RHIOffset2D 
+{
+    int32_t x;
+    int32_t y;
+};
+
+struct ST_RHIExtent2D 
+{
+    uint32_t width;
+    uint32_t height;
+};
+
+struct ST_QueueFamilyIndices
+{
+    std::optional<uint32_t> graphics_family;
+    std::optional<uint32_t> present_family;
+    std::optional<uint32_t> m_compute_family;
+
+    bool isComplete() { return graphics_family.has_value() && present_family.has_value() && m_compute_family.has_value();; }
+};
+
+struct ST_RHISwapChainDesc
+{
+    ST_RHIExtent2D extent;
+    ERHIFormat   image_format;
+    ST_RHIViewport* viewport;
+    ST_RHIRect2D* scissor;
+    std::vector<RHIImageView*> imageViews;
+};
+
+struct ST_RHIViewport
+{
+    float x;
+    float y;
+    float width;
+    float height;
+    float minDepth;
+    float maxDepth;
+};
+
+struct ST_RHIDepthImageDesc
+{
+    RHIImage* depth_image = VK_NULL_HANDLE;
+    RHIImageView* depth_image_view = VK_NULL_HANDLE;
+    ERHIFormat        depth_image_format;
+};
 
 NAMESPACE_XYH_END
