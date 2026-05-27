40914bc0a63e67929a384e3d62ca0d8952ce3965
HUAWEI\Administrator <945173861@qq.com>
2026-05-27 18:17:19 +0800

    feat: 学习代码

---
+++ Engine/Shader/hlsl/NDCTriangle.hlsl
@@ -0,0 +1,26 @@
+struct VertexData
+{
+    float4 postion : POSITIONT;
+    float4 texcoord : TEXCOORD0;
+    float4 normal : NORMAL;
+};
+
+struct VSOut
+{
+    float4 position : SV_Position;
+    float4 color : TEXCOORD0;
+};
+
+VSOut MainVS(VertexData inVertexData)
+{
+    VSOut vsOut;
+    vsOut.position = inVertexData.postion;
+    vsOut.color = inVertexData.texcoord;
+    
+    return vsOut;
+}
+
+float4 MainPS(VSOut inPSInput) : SV_Target
+{
+    return inPSInput.color;
+}
